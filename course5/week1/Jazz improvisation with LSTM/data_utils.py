deee1af18d684e879e6229bac3b579badfd86e33
Niloufar <NilouRahimi2376>
2022-12-19 09:17:55 +0330

    28Azar

---
+++ course5/week1/Jazz improvisation with LSTM/data_utils.py
@@ -0,0 +1,21 @@
+from music_utils import * 
+from preprocess import * 
+from tensorflow.keras.utils import to_categorical
+
+from collections import defaultdict
+from mido import MidiFile
+from pydub import AudioSegment
+from pydub.generators import Sine
+import math
+
+n_a = 64
+x_initializer = np.zeros((1, 1, 90))
+a_initializer = np.zeros((1, n_a))
+c_initializer = np.zeros((1, n_a))
+
+def load_music_utils(file):
+    chords, abstract_grammars = get_musical_data(file)
+    corpus, tones, tones_indices, indices_tones = get_corpus_data(abstract_grammars)
+    N_tones = len(set(corpus))
+    X, Y, N_tones = data_processing(corpus, tones_indices, 60, 30)   
+    return (X, Y, N_tones, indices_tones, chords)
